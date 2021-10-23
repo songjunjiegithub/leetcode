c64c5468c0e38730789c7f1caf00ae6f3bfdf3cf
songjunjiegithub <2401950367@qq.com>
2021-10-23 20:31:54 +0800

    add random

--- src/test/Test.java
+++ src/test/Test.java
@@ -5,7 +5,12 @@ import javacode.TreeNode;
 import java.util.*;
 
 
+/**
+ * @author junjiesong
+ */
 public class Test {
     public static void main(String[] args) {
+        Random rand = new Random();
+        System.out.println(rand.nextInt(100));
     }
 }
