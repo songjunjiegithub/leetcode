57e7837a1ada80d4a51891fdb0f244ec2a0d98fa
songjunjiegithub <2401950367@qq.com>
2021-10-23 08:28:54 +0800

    test from 2021-10-23 08:28

--- src/test/Test.java
+++ src/test/Test.java
@@ -6,16 +6,6 @@ import java.util.*;
 
 
 public class Test {
-
-
-
-
     public static void main(String[] args) {
-        Test test = new Test(3, 1);
-        System.out.println(Arrays.toString(test.flip()));
-        System.out.println(Arrays.toString(test.flip()));
-        System.out.println(Arrays.toString(test.flip()));
-        test.reset();
-        System.out.println(Arrays.toString(test.flip()));
     }
 }
